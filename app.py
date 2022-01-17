acc2f1eb2b06f25b2e0bf1a7c581804d5a6a4df6
Merten Fermont <merten.fermont@gmail.com>
2022-01-17 15:17:28 +0100

    Create disk space info

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import socket
-import sys
 import pickle
+import shutil
 
 from flask import Flask, render_template, request
 
@@ -11,52 +11,57 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    total, used, free = shutil.disk_usage('/')
+    disk_used = used / (10 ** 9)
+    disk_total = total / (10**9)
+    return render_template('index.html', disk_used=disk_used, disk_total=disk_total)
 
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
-    if request.method == 'POST':
-        app.logger.info(request.form)
-        fields = ('name', 'latitude', 'longitude', 'zoom', 'start_datetime', 'end_datetime', 'interval')
-        errors = []
-        for field in fields:
-            app.logger.info(f'{request.form[field] =} {type(request.form.get(field))}')
-            if not request.form[field]:
-                errors.append(f'{field} is niet doorgegeven!')
+    if request.method == 'GET':
+        return index()
 
-        name = request.form['name']
-        try:
-            lat = float(request.form['latitude'])
-            long = float(request.form['longitude'])
-            zoom = float(request.form['zoom'])
-            start_datetime = datetime.datetime.fromisoformat(request.form['start_datetime'])
-            end_datetime = datetime.datetime.fromisoformat(request.form['end_datetime'])
-            interval = int(request.form['interval'])
-        except ValueError as e:
-            return render_template(
-                'error.html',
-                info_text='Gegevens zijn niet juist ingevoerd, er was een fout bij de omzetting. Heb je een comma gebruikt in plaats van een punt?',
-                error=str(e))
+    app.logger.info(request.form)
+    fields = ('name', 'latitude', 'longitude', 'zoom', 'start_datetime', 'end_datetime', 'interval')
+    errors = []
+    for field in fields:
+        app.logger.info(f'{request.form[field] =} {type(request.form.get(field))}')
+        if not request.form[field]:
+            errors.append(f'{field} is niet doorgegeven!')
 
-        data_dict = {
-            'name': name,
-            'latitude': lat,
-            'longitude': long,
-            'zoom': zoom,
-            'start_datetime': start_datetime,
-            'end_datetime': end_datetime,
-            'interval': interval
-        }
-        try:
-            send_data(pickle.dumps(data_dict))
-        except Exception as e:
-            return render_template(
-                'error.html',
-                info_text='Probleem bij het verbinden met de taak-server',
-                error=repr(e))
+    name = request.form['name']
+    try:
+        lat = float(request.form['latitude'])
+        long = float(request.form['longitude'])
+        zoom = float(request.form['zoom'])
+        start_datetime = datetime.datetime.fromisoformat(request.form['start_datetime'])
+        end_datetime = datetime.datetime.fromisoformat(request.form['end_datetime'])
+        interval = int(request.form['interval'])
+    except ValueError as e:
+        return render_template(
+            'error.html',
+            info_text='Gegevens zijn niet juist ingevoerd, er was een fout bij de omzetting. Heb je een comma gebruikt in plaats van een punt?',
+            error=str(e))
 
-        return render_template('submit.html')
+    data_dict = {
+        'name': name,
+        'latitude': lat,
+        'longitude': long,
+        'zoom': zoom,
+        'start_datetime': start_datetime,
+        'end_datetime': end_datetime,
+        'interval': interval
+    }
+    try:
+        send_data(pickle.dumps(data_dict))
+    except Exception as e:
+        return render_template(
+            'error.html',
+            info_text='Probleem bij het verbinden met de taak-server',
+            error=repr(e))
+
+    return render_template('submit.html')
 
 
 def send_data(data):
